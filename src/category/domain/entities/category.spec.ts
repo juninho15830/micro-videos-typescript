dddeb68aef23c45a3c488c333039ab287b48e578
jair <juninho15830@gmail.com>
2023-11-24 15:16:22 -0300

    create setter test

--- src/category/domain/entities/category.spec.ts
+++ src/category/domain/entities/category.spec.ts
@@ -15,17 +15,18 @@ describe("Category Unit Tests", () => {
         expect(category.props.created_at).toBeInstanceOf(Date)
         
         //teste 2
+        let created_at = new Date();
         category = new Category({
             name: "Movie",
             description: "some description",
             is_active: false,
+            created_at,
         });
-        let created_at = new Date();
         expect(category.props).toStrictEqual({
             name: "Movie",
             description: "some description",
             is_active: false,
-            created_at
+            created_at,
         });
 
 
@@ -61,5 +62,69 @@ describe("Category Unit Tests", () => {
             name: "Movie",
             created_at,
         });
+    });
+
+    test("getter of name prop", () => {
+        const category = new Category({ name: "Movie"});
+        expect(category.name).toBe("Movie");
+    });
+
+    test('getter and setter of description prop', () => {
+        let category = new Category({
+            name: 'Movie',
+        });
+        expect(category.description).toBeNull();
+        
+        category = new Category({
+            name: 'Movie',
+            description: "some description",
+        });
+        expect(category.description).toBe('some description');
+
+        category = new Category({
+            name: 'Movie',
+        });
+
+        category["description"] = "other description";
+        expect(category.description).toBe('other description');
+
+        category["description"] = undefined;
+        expect(category.description).toBeNull();
+
+        category["description"] = null;
+        expect(category.description).toBeNull();
+    });
+
+    test('getter and setter of is_active prop', () => {
+        let category = new Category({
+            name: "Movie",
+        });
+        expect(category.is_active).toBeTruthy();
+
+        category = new Category({
+            name:"Movie",
+            is_active: true,
+        });
+        expect(category.is_active).toBeTruthy();
+
+        category = new Category({
+            name:"Movie",
+            is_active: false,
+        });
+        expect(category.is_active).toBeFalsy();
+    });
+
+    test('getter of crated_at prop', () => {
+        let category = new Category({
+            name: "Movie",
+        });
+        expect(category.created_at).toBeInstanceOf(Date);
+
+        let created_at = new Date();
+        category = new Category({
+            name:"Movie",
+            created_at,
+        });
+        expect(category.created_at).toBe(created_at);
     });
 });
